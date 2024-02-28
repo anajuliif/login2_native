44678225d53a5b637c53a40320a930ec3180b68a
anajuliif <anafeereira125@gmail.com>
2024-02-28 15:40:54 -0300

    commit

---
+++ src/components/LoginInput.js
@@ -0,0 +1,9 @@
+import { Text, View, StyleSheet } from "react-native";
+import styles from "../styles/StyleSheet";
+export default function Componente1() {
+  return (
+    <View style={styles.cabecalho}>
+      <TextInput>Componente 1 </TextInput>
+    </View>
+  );
+}
---
+++ src/styles/StyleSheet.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    inputname: {
+      color: 'black',
+      borderWidth: 1,
+      borderColor: 'black',
+      marginBottom: 10,
+      width: 300,
+      height: 50,
+      backgroundColor: 'white',
+      borderRadius: 10, // Adicionando bordas arredondadas
+      paddingHorizontal: 10, // Espaçamento interno horizontal
+    }
+})
